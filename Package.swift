1c10c49176383a0a02948ff84377e7f5b3f2e049
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-03-11 10:57:06 -0500

    Package platform support

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "UnifiedLoggingPlus",
+    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
     products: [
         .library(name: "UnifiedLoggingPlus", targets: ["UnifiedLoggingPlus"]),
     ],
