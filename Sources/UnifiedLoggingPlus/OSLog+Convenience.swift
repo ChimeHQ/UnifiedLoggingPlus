326b0dce73aef3b8e4e71d454127d9187d0c9cd1
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-03-11 11:17:46 -0500

    Zero-arg overloads

--- Sources/UnifiedLoggingPlus/OSLog+Convenience.swift
+++ Sources/UnifiedLoggingPlus/OSLog+Convenience.swift
@@ -5,6 +5,10 @@ import os.log
 
 @available(macOS 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)
 public extension OSLog {
+    func debug(_ message: StaticString) {
+        os_log(message, log: self, type: .debug)
+    }
+
     func debug(_ message: StaticString, _ arg1: CVarArg) {
         os_log(message, log: self, type: .debug, arg1)
     }
@@ -15,6 +19,10 @@ public extension OSLog {
 
     func debug(_ message: StaticString, _ arg1: CVarArg, _ arg2: CVarArg, _ arg3: CVarArg) {
         os_log(message, log: self, type: .debug, arg1, arg2, arg3)
+    }
+
+    func info(_ message: StaticString) {
+        os_log(message, log: self, type: .info)
     }
 
     func info(_ message: StaticString, _ arg1: CVarArg) {
@@ -29,6 +37,10 @@ public extension OSLog {
         os_log(message, log: self, type: .info, arg1, arg2, arg3)
     }
 
+    func error(_ message: StaticString) {
+        os_log(message, log: self, type: .error)
+    }
+
     func error(_ message: StaticString, _ arg1: CVarArg) {
         os_log(message, log: self, type: .error, arg1)
     }
@@ -39,6 +51,10 @@ public extension OSLog {
 
     func error(_ message: StaticString, _ arg1: CVarArg, _ arg2: CVarArg, _ arg3: CVarArg) {
         os_log(message, log: self, type: .error, arg1, arg2, arg3)
+    }
+
+    func fault(_ message: StaticString) {
+        os_log(message, log: self, type: .fault)
     }
 
     func fault(_ message: StaticString, _ arg1: CVarArg) {
--- Tests/UnifiedLoggingPlusTests/OSLogTests.swift
+++ Tests/UnifiedLoggingPlusTests/OSLogTests.swift
@@ -6,6 +6,7 @@ class OSLogTests: XCTestCase {
     func testDebugLogging() {
         let log = OSLog(subsystem: "UnifiedLoggingPlusTests", category: "tests")
 
+        log.debug("test")
         log.debug("test %@", "arg1")
         log.debug("test %@, %@", "arg1", "arg2")
         log.debug("test %@, %@, %@", "arg1", "arg2", "arg3")
